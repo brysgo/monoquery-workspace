6ee8d25d3585d32f6c2de58243df92b499f0fe97
Bryan Goldstein <brysgo@gmail.com>
2018-04-29 18:30:22 -0400

    add decorator for provider side aswell

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,7 +2,6 @@ import { TestBed, fakeAsync, tick } from "@angular/core/testing";
 
 import { ExampleComponent } from "./example/ExampleComponent";
 import { MonoProvider } from "./example/MonoProvider";
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 describe("MonoQueryModule", () => {
   it(
@@ -16,6 +15,7 @@ describe("MonoQueryModule", () => {
       const monoProvider = TestBed.get(MonoProvider);
       const fixture = TestBed.createComponent(ExampleComponent);
       fixture.detectChanges();
+      tick();
       expect(fixture).toMatchSnapshot();
     })
   );
